8a5b49cfe261608790c1fcc777d7eff7861d650b
Bruno Bilandzic <bruno.bilandzic2@gmail.com>
2023-03-27 13:31:56 +0200

    tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <h1 className="text-red-700">Home</h1>
     </main>
   );
 }
