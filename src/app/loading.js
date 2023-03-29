a230b5b98282e8bf2d93d74abf0a7b6276b74fac
Bruno Bilandzic <bruno.bilandzic2@gmail.com>
2023-03-29 12:26:14 +0200

    add loading

---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+      <div className='flex justify-center '>
+          <img src="spinner.svg" alt="loading" className='h-96'/>
+    </div>
+  )
+}
