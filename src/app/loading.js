01a6fb72fb33adc612a4ac35cff27f299cec8cbb
Bruno Bilandzic <bruno.bilandzic2@gmail.com>
2023-03-30 13:40:10 +0200

    quickfix

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Loading() {
   return (
       <div className='flex justify-center '>
-          <img src="spinner.svg" alt="loading" className='h-96'/>
+          <Image src="spinner.svg" alt="loading" className='h-96'/>
     </div>
   )
 }
